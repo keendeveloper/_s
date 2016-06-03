5873d6f82340c12115fd7eea97abc3fce4764667
Thomas Sultana <developer@keen-advertising.com>
2016-06-03 18:04:30 +0200

    Added ajax sample. Updated metabox invocation code.

---
+++ inc/_s-ajax.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+add_action( 'wp_ajax_add_foobar', '_s_ajax_add_foobar' );
+add_action( 'wp_ajax_nopriv_add_foobar', '_s_ajax_add_foobar' );
+
+function _s_ajax_add_foobar() {
+    
+}
+*/
